8142c0569a26c018c3fb27ccf5b4ac6c88563bbd
Mayank Mittal <mayankm.iitk@gmail.com>
2018-05-17 23:26:39 +0530

    updated build script from latest airsim/build.sh

--- build.sh
+++ build.sh
@@ -1,45 +1,34 @@
 #! /bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
+pushd "$SCRIPT_DIR"  >/dev/null
 
 set -e
 
-# update the rpclib git submodule
-git submodule update --init --recursive 
+# check for rpclib
+if [ ! -d "./external/rpclib/rpclib-2.2.1" ]; then
+    echo "ERROR: new version of AirSim requires newer rpclib."
+    echo "please run setup.sh first and then run build.sh again."
+    exit 1
+fi
 
-#!/bin/bash
-# if [[ !(-f "/usr/bin/clang") || !(-f "/usr/bin/clang++") ]]; then
-# 	echo "ERROR: clang++ 3.9 is necessary to compile AirSim and run it in Unreal"
-# 	echo "       please run : sudo apt-get install clang++-3.9"
-# 	echo "       followed by: sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.9 60 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.9 "
+# check for cmake build
+if [ ! -d "./cmake_build" ]; then
+    echo "ERROR: cmake build was not found."
+    echo "please run setup.sh first and then run build.sh again."
+    exit 1
+fi
 
-#   echo "       We need to use clang because the Unreal Engine is built with clang as well and"
-#   echo "       there are some symbol inconsistencies in the C++ library with regard to C++11"
-#   echo "       (see GCC Dual ABI: # https://gcc.gnu.org/onlinedocs/libstdc++/manual/using_dual_abi.html)"
-
-# 	exit 1
-# fi
-
-#export CC=/usr/bin/clang
-#export CXX=/usr/bin/clang++
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-if [[ ! -d "$EIGEN_ROOT" ]]; then 
-	if [[ ! -d eigen ]]; then
-		echo "downloading eigen..."
-		wget http://bitbucket.org/eigen/eigen/get/3.3.2.zip
-		unzip 3.3.2.zip -d eigen
-		pushd eigen
-		mv eigen* eigen3
-		echo "3.3.2" > version
-		popd &>/dev/null
-		rm 3.3.2.zip
-	fi
-	export EIGEN_ROOT="$(pwd)/eigen"
+#install EIGEN library
+if [[ !(-d "./AirLib/deps/eigen3/Eigen") ]]; then 
+    echo "eigen is not installed. Please run setup.sh first."
+    exit 1
 fi
 
+# variable for build output
 build_dir=build_debug
 echo "putting build in build_debug folder, to clean, just delete the directory..."
 
@@ -52,20 +41,20 @@ if [[ -d "./cmake/CMakeFiles" ]]; then
 fi
 
 if [[ ! -d $build_dir ]]; then
-	mkdir -p $build_dir
-	pushd $build_dir
+    mkdir -p $build_dir
+    pushd $build_dir  >/dev/null
 
-	cmake ../cmake -DCMAKE_BUILD_TYPE=Debug \
-		|| (cd .. && rm -r $build_dir && exit 1)
-	popd &>/dev/null
+    "$CMAKE" ../cmake -DCMAKE_BUILD_TYPE=Debug \
+        || (popd && rm -r $build_dir && exit 1)
+    popd >/dev/null
 fi
 
-pushd $build_dir
+pushd $build_dir  >/dev/null
 # final linking of the binaries can fail due to a missing libc++abi library
 # (happens on Fedora, see https://bugzilla.redhat.com/show_bug.cgi?id=1332306).
 # So we only build the libraries here for now
 make 
-popd &>/dev/null
+popd >/dev/null
 
 
 mkdir -p AirLib/lib/x64/Debug
@@ -74,18 +63,29 @@ mkdir -p AirLib/deps/MavLinkCom/lib
 cp $build_dir/output/lib/libAirLib.a AirLib/lib
 cp $build_dir/output/lib/libMavLinkCom.a AirLib/deps/MavLinkCom/lib
 cp $build_dir/output/lib/librpc.a AirLib/deps/rpclib/lib/librpc.a
+
+# Update AirLib/lib, AirLib/deps, Plugins folders with new binaries
 rsync -a --delete $build_dir/output/lib/ AirLib/lib/x64/Debug
 rsync -a --delete external/rpclib/rpclib-2.2.1/include AirLib/deps/rpclib
 rsync -a --delete MavLinkCom/include AirLib/deps/MavLinkCom
 rsync -a --delete AirLib Unreal/Plugins/AirSim/Source
 
+# Update Blocks project
+Unreal/Environments/Blocks/clean.sh
+mkdir -p Unreal/Environments/Blocks/Plugins
+rsync -a --delete Unreal/Plugins/AirSim Unreal/Environments/Blocks/Plugins
+
+set +x
 
 echo ""
-echo "============================================================"
-echo "Now copy the Unreal/Plugins directory to the Unreal project:"
-echo "rsync -t -r Unreal/Plugins <unreal project_root>"
-echo "  (<unreal project_root> contains a file named <project>.uproject)"
-echo "============================================================"
-echo "And do (required for building the Unreal plugin):"
-echo "export EIGEN_ROOT=\"$EIGEN_ROOT\""
-
+echo ""
+echo "=================================================================="
+echo " AirSim plugin is built! Here's how to build Unreal project."
+echo "=================================================================="
+echo "If you are using Blocks environment, its already updated."
+echo "If you are using your own environment, update plugin using,"
+echo "rsync -t -r Unreal/Plugins path/to/MyUnrealProject"
+echo ""
+echo "For help see:"
+echo "https://github.com/Microsoft/AirSim/blob/master/docs/build_linux.md"
+echo "=================================================================="
